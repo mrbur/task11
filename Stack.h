17a5227e621db371b2d9224c8908a1ddf8dbccfc
Andrei <burykinmail@gmail.com>
2021-07-01 20:30:44 +0300

    variables name refactoring, added destructor

--- Stack.h
+++ Stack.h
@@ -5,39 +5,42 @@ using namespace std;
 
 class Stack {
 	private:
-		const int n;
+		const int maxSize;
 		int* arr;
-		size_t arrSize = 0;
+		size_t stackSize = 0;
 
 	public:
-		Stack() : n(10) {
-			arr = new int[n];
+		Stack() : maxSize(10) {
+			arr = new int[maxSize];
+		}
+		~Stack() {
+			delete[] arr;
 		}
 		void reset() {
-			for (size_t i = 0; i < arrSize; i++) {
+			for (size_t i = 0; i < stackSize; i++) {
 				arr[i] = 0;
 			}
-			arrSize = 0;
+			stackSize = 0;
 		}
 
 		bool push(int val) {
-			if (arrSize == n) return false;
-			arr[arrSize++] = val;
+			if (stackSize == maxSize) return false;
+			arr[stackSize++] = val;
 			return true;
 		}
 
 		int pop() {
-			if (arrSize == 0) {
+			if (stackSize == 0) {
 				cout << "Stack is empty";
 			}
 			else {
-				return arr[arrSize--];
+				return arr[stackSize--];
 			}
 		}
 
 		void print() {
 			cout << "(";
-			for (size_t i = 0; i < arrSize; i++) {
+			for (size_t i = 0; i < stackSize; i++) {
 				cout << " " << arr[i];
 			}
 			cout << " )";
