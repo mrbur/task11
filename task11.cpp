813fe93abf18bbeb44ab690958321ee76aea2509
Andrei <burykinmail@gmail.com>
2021-07-01 20:34:15 +0300

    clear method name

--- task11.cpp
+++ task11.cpp
@@ -1,24 +1,7 @@
 ﻿#include <iostream>
-#include "Stack.h"
 
 int main()
 {
-    Stack stack;
-    stack.reset();
-    stack.print();
-
-    stack.push(3);
-    stack.push(7);
-    stack.push(5);
-    stack.print();
-
-    stack.pop();
-    stack.print();
-
-    stack.pop();
-    stack.pop();
-    stack.print();
-
     return 0;
 }
 
